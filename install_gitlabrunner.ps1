999171ffe35aa7e35c3a3dec984e7fe739aaeef8
qiaofeng1227 <76487013@qq.com>
2021-12-22 14:49:46 +0800

    Update install_gitlabrunner.ps1

--- install_gitlabrunner.ps1
+++ install_gitlabrunner.ps1
@@ -1,1 +1,2 @@
-
+Invoke-WebRequest -URI “https://github.com/git-for-windows/git/releases/download/v2.34.1.windows.1/Git-2.34.1-64-bit.exe” -OutFile git.exe
+Start-Process git.exe -ArgumentList '--installPath C:\git --quiet --norestart' -wait -NoNewWindow
