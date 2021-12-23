6022b3c471593b8aa5f39f7dd86a56b0b450bf83
chendelin1982 <27513732@qq.com>
2021-12-23 11:35:08 +0800

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -5,12 +5,9 @@ Start-Process -FilePath "git.exe" -ArgumentList " /SP- /VERYSILENT /SUPPRESSMSGB
 
 
 # Install GitLab-runner
-$password=Get-Random
-echo $password | out-file -filepath password.txt
-
+$runner_url="https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-windows-amd64.exe"
 new-item GitLab-Runner -type directory
 cd GitLab-Runner
-$runner_url="https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-windows-amd64.exe"
 Invoke-WebRequest -URI $runner_url -OutFile gitlab-runner.exe
-.\gitlab-runner.exe install --user root --password $password
+.\gitlab-runner.exe install
 .\gitlab-runner.exe start
