07d8ca04400a26a80b19c083505dee81bf4f049e
chendelin1982 <27513732@qq.com>
2021-12-23 09:58:46 +0800

    Update install_gitlabrunner.ps1

--- install_gitlabrunner.ps1
+++ install_gitlabrunner.ps1
@@ -1,8 +1,10 @@
-# Install git
-#Invoke-WebRequest -URI "https://github.com/git-for-windows/git/releases/download/v2.34.1.windows.1/PortableGit-2.34.1-64-bit.7z.exe" -OutFile git.exe
-#Start-Process git.exe -ArgumentList '--installPath C:\Program Files\Git --quiet --norestart' -wait -NoNewWindow
+# Install Git
+$git_url="https://github.com/git-for-windows/git/releases/download/v2.34.1.windows.1/Git-2.34.1-64-bit.exe"
+Invoke-WebRequest -Uri $git_url  -OutFile git.exe
+Start-Process -FilePath "git.exe" -ArgumentList " /SP- /VERYSILENT /SUPPRESSMSGBOXES /FORCECLOSEAPPLICATIONS" -Wait
 
-# Install gitlab-runner
-Invoke-WebRequest -URI "https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-windows-amd64.exe" -OutFile gitlab-runner.exe
+# Install GitLab-runner
+$gitlab_url="https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-windows-amd64.exe"
+Invoke-WebRequest -URI $gitlab_url -OutFile gitlab-runner.exe
 .\gitlab-runner.exe install --user root --password yourpassword
 .\gitlab-runner.exe start
