ef467408b70093bc1c3cb502874c3bc9bc1edae2
qiaofeng1227 <76487013@qq.com>
2022-01-06 10:51:56 +0800

    Update runner-config.ps1

--- runner-config.ps1
+++ runner-config.ps1
@@ -1,7 +1,7 @@
 # Configure image runner
-.\gitlab-runner.exe register --run-untagged --non-interactive --url "http://43.154.150.20/" --registration-token "sjCzRqVb5WkTP_WJ6bpM" --executor "shell"
+.\gitlab-runner.exe register --run-untagged --non-interactive --url "http://43.154.150.20/" --registration-token "sjCzRqVb5WkTP_WJ6bpM" --executor "shell" --shell "powershell"
 
 # Configure build-project runner
-.\gitlab-runner.exe register --run-untagged --non-interactive --url "http://43.154.150.20/" --registration-token "HKYW6jq8Z7EroY_zqpLk" --executor "docker-windows" --docker-image "plcn" --docker-pull-policy "if-not-present"
+.\gitlab-runner.exe register --run-untagged --non-interactive --url "http://43.154.150.20/" --registration-token "HKYW6jq8Z7EroY_zqpLk" --executor "docker-windows"  --shell "powershell"--docker-image "plcn" --docker-pull-policy "if-not-present"
 
 .\gitlab-runner.exe restart
