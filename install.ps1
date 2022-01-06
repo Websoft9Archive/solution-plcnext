35f5db68ef337f8ce51d3b25a7e2d19a85f0417e
qiaofeng1227 <76487013@qq.com>
2022-01-06 10:30:16 +0800

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -12,13 +12,7 @@ Invoke-WebRequest -URI $runner_url -OutFile gitlab-runner.exe
 .\gitlab-runner.exe install
 .\gitlab-runner.exe start
 
-# Configure image runner
-.\gitlab-runner.exe register --run-untagged --non-interactive --url "http://43.154.150.20/" --registration-token "sjCzRqVb5WkTP_WJ6bpM" --executor "shell"
 
-# Configure build-project runner
-.\gitlab-runner.exe register --run-untagged --non-interactive --url "http://43.154.150.20/" --registration-token "HKYW6jq8Z7EroY_zqpLk" --executor "docker-windows" --docker-image "plcn" --docker-pull-policy "if-not-present"
-
-.\gitlab-runner.exe restart
  
  
 
